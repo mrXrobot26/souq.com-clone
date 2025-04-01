737fce3cad1b516fb6533eb4a1a9254d43c2bd85
mrXrobot26 <zezoelbessa10@gmail.com>
2025-04-01 09:23:34 +0200

    add dotenv and morgan dependencies, and enhance server logging for development 1- npm i dotenv 2- npm i morgan

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const env = require('dotenv')
+const morgen = require('morgan')
+env.config();
 
 
 const app = express();
 
-app.listen(8000 , ()=> {
-    console.log(`app runinng on port 8000`)
+if(process.env.NODE_ENV == 'development'){
+    console.log(`Mode : ${process.env.NODE_ENV}`)
+    app.use(morgen('dev'))
+    console.log("======================================")
+}
+
+const port = process.env.PORT || 3000;
+
+app.listen(port , ()=> {
+    console.log(`app runinng on port : ${port}`)
 })
 
 app.get('/',(req , res) =>
 {
-    res.json("our api")
+    res.json("our api v3")
 })
