3a7daea9b55643e1663d296e88f21c95f99c5f21
mrXrobot26 <zezoelbessa10@gmail.com>
2025-04-01 10:51:59 +0200

    add mongoose dependency and implement database connection in server 1- npm install mongoose --save

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const env = require('dotenv')
 const morgen = require('morgan')
+const mongoose = require('mongoose')
+
+
+
 env.config();
-
-
 const app = express();
+
+
+db_connect().then(() => console.log("Database connected successfully")).catch((err) => console.error("Database error:", err));
+async function db_connect() {
+    await mongoose.connect(process.env.db_application);
+}
 
 if(process.env.NODE_ENV == 'development'){
     console.log(`Mode : ${process.env.NODE_ENV}`)
@@ -13,7 +21,6 @@ if(process.env.NODE_ENV == 'development'){
 }
 
 const port = process.env.PORT || 3000;
-
 app.listen(port , ()=> {
     console.log(`app runinng on port : ${port}`)
 })
