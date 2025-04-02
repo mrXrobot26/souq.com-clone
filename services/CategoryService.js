8cd2b235c89cbdfbf8f2a9a6ebd2d844a0e8eec4
mrXrobot26 <zezoelbessa10@gmail.com>
2025-04-02 08:07:40 +0200

    add express-async-handler for improved error handling in AddCategory service 1- npm i express-async-handler

--- services/CategoryService.js
+++ services/CategoryService.js
@@ -1,20 +1,16 @@
 const CategoryModel = require('../models/categoryModel');
 const slugify = require('slugify');
-
-const AddCategory = async (req, res) => {
+const asyncHandler = require('express-async-handler')
+const AddCategory = asyncHandler(async (req, res) => {
     const nameFromReq = req.body.name;
-    await CategoryModel.create({
+    const category = await CategoryModel.create({
         name: nameFromReq,
         slag: slugify(nameFromReq)
     })
-    .then( category => res.status(201).json({
+    res.status(201).json({
         status: 'success',
         data: category
-    }))
-    .catch(err => res.status(500).json({
-        status: 'error',
-        message: err.message,
-        code: err.code
-    }))
-};
+    })
+});
 module.exports = AddCategory;
+
