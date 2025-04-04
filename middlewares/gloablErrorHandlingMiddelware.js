d0d2a233b28f84d4dc34a1746c7d25254197bf54
mrXrobot26 <zezoelbessa10@gmail.com>
2025-04-04 06:50:55 +0200

    refactor global error handling middleware for improved environment-specific responses; add cross-env for production script 1-npm install cross-env --save-dev

--- middlewares/gloablErrorHandlingMiddelware.js
+++ middlewares/gloablErrorHandlingMiddelware.js
@@ -1,13 +1,28 @@
-const globalError = (err, req, res, _next) => {
+const globalError = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
+    if (process.env.NODE_ENV === 'development') {
+        return globalErrorOnDev(err, res);
+    } else {
+        return globalErrorOnProd(err, res);
+    }
+};
+
+const globalErrorOnDev = (err, res) => {
     res.status(err.statusCode).json({
-        statusCode : err.statusCode,
+        statusCode: err.statusCode,
         status: err.status,
         message: err.message,
-        stack: err.stack 
+        stack: err.stack
     });
-}
+};
 
-module.exports = globalError
+const globalErrorOnProd = (err, res) => {
+    res.status(err.statusCode).json({
+        statusCode: err.statusCode,
+        message: err.message
+    });
+};
+
+module.exports = globalError;
