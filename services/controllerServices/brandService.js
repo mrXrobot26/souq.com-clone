63ca758283393b3778913047eeeedc5b251128e8
mrXrobot26 <zezoelbessa10@gmail.com>
2025-04-19 14:00:11 +0200

    refactor(brandService): replace CRUD operations with factory handler
    This commit refactors the brandService to use the newly introduced factoryHandler for CRUD operations. This improves code maintainability and reduces redundancy by centralizing common database operations in a reusable utility.

--- services/controllerServices/brandService.js
+++ services/controllerServices/brandService.js
@@ -1,109 +1,40 @@
 const { default: slugify } = require("slugify");
 const Brand = require("../../models/brandModel");
-const APIError = require("../../utils/APIError");
 const asyncHandler = require("express-async-handler");
+const factoryHandler = require("../../utils/factoryHandler");
 
-
-/**
- * Creates a new brand
- * @param {string} nameFromController - The name of the brand to create
- * @returns {Object} - Object containing the created brand
- */
 const createBrand = asyncHandler(async (nameFromController) => {
-  const brandToDb = await Brand.create({
+  const brandData = {
     name: nameFromController,
     slug: slugify(nameFromController, { lower: true, strict: true }),
-  });
-
-  return {
-    data: brandToDb,
   };
+  return await factoryHandler.createOne(Brand)(brandData);
 });
 
-/**
- * Gets a brand by ID
- * @param {string} idFromController - The ID of the brand to fetch
- * @returns {Object} - Object containing the brand data
- */
 const getBrand = asyncHandler(async (idFromController) => {
-  const brandFromDb = await Brand.findById(idFromController);
-
-  if (!brandFromDb) {
-    throw new APIError("Brand not found", 404);
-  }
-
-  return {
-    data: brandFromDb,
-  };
+  return await factoryHandler.getOne(Brand, "Brand")(idFromController);
 });
 
-/**
- * Gets a paginated list of brands with optional filtering
- * @param {Object} req - Express request object containing query parameters
- * @returns {Object} - Paginated results with metadata
- */
 const getBrands = asyncHandler(async (req) => {
-  const APIFeatures = require("../../utils/APIFeatures");
-  
-  // Initialize APIFeatures with Brand model and request query
-  const features = new APIFeatures(Brand.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate()
-    .search(['name']);
-
-  
-  // Execute query
-  const brandsFromDb = await features.query;
-  
-  // Get total count for pagination
-  const count = await Brand.countDocuments(features.query.getFilter());
-  
-  // Return formatted response with pagination metadata
-  return features.formatResponse(brandsFromDb, count);
+  return await factoryHandler.getAll(Brand, ["name"])(req);
 });
 
-/**
- * Updates a brand by ID
- * @param {string} idFromController - The ID of the brand to update
- * @param {string} nameFromController - The new name for the brand
- * @returns {Object} - Object containing the updated brand
- */
-const updateBrand = asyncHandler(async (idFromController, nameFromController) => {
-  const brand = await Brand.findOneAndUpdate(
-    { _id: idFromController },
-    { 
-      name: nameFromController, 
-      slug: slugify(nameFromController, { lower: true, strict: true }) 
-    },
-    { new: true, runValidators: true }
-  );
+const updateBrand = asyncHandler(
+  async (idFromController, nameFromController) => {
+    const updateData = {
+      name: nameFromController,
+      slug: slugify(nameFromController, { lower: true, strict: true }),
+    };
 
-  if (!brand) {
-    throw new APIError("Brand not found", 404);
+    return await factoryHandler.updateOne(Brand, "Brand")(
+      idFromController,
+      updateData
+    );
   }
+);
 
-  return {
-    data: brand,
-  };
-});
-
-/**
- * Deletes a brand by ID
- * @param {string} idFromController - The ID of the brand to delete
- * @returns {Object} - Success message
- */
 const deleteBrand = asyncHandler(async (idFromController) => {
-  const brand = await Brand.findByIdAndDelete(idFromController);
-
-  if (!brand) {
-    throw new APIError("Brand not found", 404);
-  }
-
-  return {
-    message: "Brand deleted successfully",
-  };
+  return await factoryHandler.deleteOne(Brand, "Brand")(idFromController);
 });
 
 module.exports = {
---
+++ utils/factoryHandler.js
@@ -0,0 +1,159 @@
+/**
+ * Factory Handler for CRUD Operations
+ * Provides reusable handler functions for common database operations
+ */
+const asyncHandler = require("express-async-handler");
+const APIError = require("./APIError");
+const APIFeatures = require("./APIFeatures");
+
+/**
+ * Factory function to delete a document by ID
+ * @param {Model} Model - Mongoose model
+ * @param {String} modelName - Name of the model for error messages
+ * @returns {Function} - Async handler function that deletes a document
+ */
+exports.deleteOne = (Model, modelName = "Document") =>
+  asyncHandler(async (id) => {
+    const document = await Model.findByIdAndDelete(id);
+
+    if (!document) {
+      throw new APIError(`${modelName} not found`, 404);
+    }
+
+    return {
+      message: `${modelName} deleted successfully`,
+    };
+  });
+
+/**
+ * Factory function to delete multiple documents by filter
+ * @param {Model} Model - Mongoose model
+ * @param {String} modelName - Name of the model for error messages
+ * @returns {Function} - Async handler function that deletes multiple documents
+ */
+exports.deleteMany = (Model, modelName = "Documents") =>
+  asyncHandler(async (filter = {}) => {
+    const result = await Model.deleteMany(filter);
+
+    return {
+      message: `${result.deletedCount} ${modelName} deleted successfully`,
+    };
+  });
+
+/**
+ * Factory function to get a document by ID
+ * @param {Model} Model - Mongoose model
+ * @param {String} modelName - Name of the model for error messages
+ * @param {Array} populateOptions - Array of populate options
+ * @returns {Function} - Async handler function that gets a document
+ */
+exports.getOne = (Model, modelName = "Document", populateOptions = []) =>
+  asyncHandler(async (id) => {
+    let query = Model.findById(id);
+
+    if (populateOptions.length > 0) {
+      populateOptions.forEach((option) => {
+        query = query.populate(option);
+      });
+    }
+
+    const document = await query;
+
+    if (!document) {
+      throw new APIError(`${modelName} not found`, 404);
+    }
+
+    return {
+      data: document,
+    };
+  });
+
+/**
+ * Factory function to get all documents with filtering, sorting, pagination
+ * @param {Model} Model - Mongoose model
+ * @param {Array} searchFields - Fields to search in
+ * @param {Array} populateOptions - Array of populate options
+ * @returns {Function} - Async handler function that gets documents
+ */
+exports.getAll = (Model, searchFields = ["name"], populateOptions = []) =>
+  asyncHandler(async (req) => {
+    const features = new APIFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .search(searchFields)
+      .paginate();
+
+    if (populateOptions.length > 0) {
+      populateOptions.forEach((option) => {
+        features.query.populate(option);
+      });
+    }
+
+    // Execute query
+    const documents = await features.query;
+
+    // Get total count for pagination
+    const count = await Model.countDocuments(features.query.getFilter());
+
+    // Return formatted response
+    return features.formatResponse(documents, count);
+  });
+
+/**
+ * Factory function to create a document
+ * @param {Model} Model - Mongoose model
+ * @param {Array} populateOptions - Array of populate options
+ * @returns {Function} - Async handler function that creates a document
+ */
+exports.createOne = (Model, populateOptions = []) =>
+  asyncHandler(async (data) => {
+    const document = await Model.create(data);
+
+    let populatedDocument = document;
+
+    if (populateOptions.length > 0) {
+      let query = Model.findById(document._id);
+
+      populateOptions.forEach((option) => {
+        query = query.populate(option);
+      });
+
+      populatedDocument = await query;
+    }
+
+    return {
+      data: populatedDocument,
+    };
+  });
+
+/**
+ * Factory function to update a document
+ * @param {Model} Model - Mongoose model
+ * @param {String} modelName - Name of the model for error messages
+ * @param {Array} populateOptions - Array of populate options
+ * @returns {Function} - Async handler function that updates a document
+ */
+exports.updateOne = (Model, modelName = "Document", populateOptions = []) =>
+  asyncHandler(async (id, data) => {
+    let query = Model.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (populateOptions.length > 0) {
+      populateOptions.forEach((option) => {
+        query = query.populate(option);
+      });
+    }
+
+    const document = await query;
+
+    if (!document) {
+      throw new APIError(`${modelName} not found`, 404);
+    }
+
+    return {
+      data: document,
+    };
+  });
